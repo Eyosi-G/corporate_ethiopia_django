19b37637fd063a987bd4806288a7f16cc55161c7
= <eyosiyasgirma0@gmail.com>
2023-03-06 13:41:50 +0300

    detail jobs page, companies and detail company ui created

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('jobs/', views.jobs, name='jobs'),
+    path('jobs/<slug:slug>/', views.job_detail),
     path('companies/', views.companies, name='companies'),
     path('companies/<slug:slug>/', views.company_detail)
 
--- main/views.py
+++ main/views.py
@@ -3,13 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'pages/home.html')
+    return render(request, 'pages/home.html', { "isHomeActive": True })
 
 def jobs(request):
-    return render(request, 'pages/jobs.html')
+    return render(request, 'pages/jobs.html', { "isJobsActive": True })
+
+def job_detail(request, slug):
+    return render(request, 'pages/job-detail.html', { 'slug': slug})
 
 def companies(request):
-    return render(request, 'pages/companies.html')
+    return render(request, 'pages/companies.html', { "isCompaniesActive":True})
 
 def company_detail(request, slug ):
     return render(request, 'pages/company-detail.html', context={ 'slug':slug })
