e2681db011ece146659458812de24a61ebb57c7b
= <eyosiyasgirma0@gmail.com>
2023-03-07 18:33:58 +0300

    mobile view

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,33 @@
-
-
 const toggleElement = (elementId, elementContentId) => {
-  const workMode = document.getElementById(elementId);
-  workMode.addEventListener("click", () => {
-    console.log('here')
-    const workModeContent = document.getElementById(elementContentId);
-    if (workModeContent.classList.contains("hidden")) {
-      workModeContent.classList.remove("hidden");
+  const element = document.getElementById(elementId);
+  console.log(element);
+  element.addEventListener("click", () => {
+    const elementContent = document.getElementById(elementContentId);
+    if (elementContent.classList.contains("hidden")) {
+      elementContent.classList.remove("hidden");
     } else {
-      workModeContent.classList.add("hidden");
+      elementContent.classList.add("hidden");
     }
   });
 };
 
-document.addEventListener('DOMContentLoaded', ()=> {
-  toggleElement("work_mode", "work_mode_content")
-  toggleElement("employement_type", "employement_type_content")
-  toggleElement("experience", "experience_content")
-  toggleElement("sector", "sector_content")
-  toggleElement("regions", "regions_content")
+document.addEventListener("DOMContentLoaded", () => {
+  toggleElement("work_mode", "work_mode_content");
+  toggleElement("employement_type", "employement_type_content");
+  toggleElement("experience", "experience_content");
+  toggleElement("sector", "sector_content");
+  toggleElement("regions", "regions_content");
 
-})
+  
+});
 
+
+const element = document.getElementById("menu_button");
+element.addEventListener("click", () => {
+  const elementContent = document.getElementById("menus");
+  if (elementContent.classList.contains("hidden")) {
+    elementContent.classList.remove("hidden");
+  } else {
+    elementContent.classList.add("hidden");
+  }
+});
